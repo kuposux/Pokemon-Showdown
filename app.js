bb0396a53729832e4f8b0b5cab55af06b01e1092
pandaw <itachi.fai@gmail.com>
2013-01-06 16:41:02 -0800

    if gitpull, then hotpatch

--- app.js
+++ app.js
@@ -341,6 +341,19 @@ runCommand = function(command, args, socket) {
 	process.nextTick(function() {
 		pushBuffer('child process exited with code ' + code);
 		emit(socket, 'console', buffer);
+		
+		//stevo was here
+		if (command == "git" && args[1] == "pull") {
+			for (var i in require.cache) delete require.cache[i];
+			Tools = require('./tools.js');
+			parseCommand = require('./chat-commands.js').parseCommand;
+			sim = require('./battles.js');
+			BattlePokemon = sim.BattlePokemon;
+			BattleSide = sim.BattleSide;
+			Battle = sim.Battle;
+			emit(socket, 'console', 'The game engine has been hot-patched.');
+			Rooms.lobby.addRaw('<div style="background:#7067AB;color:white;padding:2px 4px"><b>Server update finished.</b></div>');
+		}
 		});
 	});
 }
