5c2f614873e40d8d12ccd46917ff06042f4836a1
jd4564 <thelittlejd09@gmail.com>
2013-01-13 22:55:06 -0400

    Update chat-commands.js

--- chat-commands.js
+++ chat-commands.js
@@ -1306,7 +1306,8 @@ function parseCommandLocal(user, cmd, target, room, socket, message) {
 		var args = splitArgs('git, pull');
 		logModCommand(room,user.name+' pulled from git',true);
 		room.addRaw('<div style="background:#7067AB;color:white;padding:2px 4px"><b>Server updating... there might be some lag.</b></div>');
-		var gitpulling = true;
+		var gitpulling;
+		gitpulling = true;
 		runCommand(args.shift(), args, socket);
 		//for (var i in require.cache) delete require.cache[i];
 		//Tools = require('./tools.js');
