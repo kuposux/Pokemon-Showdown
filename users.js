8e815bd97fb0e5f6fb05635d375f636d974c235d
pandaw <itachi.fai@gmail.com>
2013-01-24 21:42:54 -0800

    dumb mistake fix x:

--- users.js
+++ users.js
@@ -482,7 +482,7 @@ var User = (function () {
 				else if (userid === "jd") avatar = 1005;
 				else if (userid === "kupo") avatar = 1011;
 				else if (userid === "lightblue")avatar = 285;
-				else if (userid === "energ218" || userid ==- 'tevos') ((Math.floor(Math.rand()*2)==0)? avatar = 1010: avatar = 1008);
+				else if (userid === "energ218" || userid == 'tevos') ((Math.floor(Math.rand()*2)==0)? avatar = 1010: avatar = 1008);
 				else if (userid === "wiitle") avatar = 218;
 				else if (userid === "loong" || userid === "chomi") avatar = 1005;
 				else if (userid === "noir") avatar = 164;
