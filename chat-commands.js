310df681ed8b4cb0e422970c77c5ef02ea234432
pandaw <itachi.fai@gmail.com>
2013-02-13 17:17:01 -0800

    fix /remind

--- chat-commands.js
+++ chat-commands.js
@@ -188,7 +188,7 @@ function parseCommandLocal(user, cmd, target, room, socket, message) {
 			emit(socket, 'console', 'The tournament is not currently in its signup phase.');
 			return false;
 		}
-		var tourName = tour.tiers[tour[roomid].tier].name; var vowels = ["A", "E", "I", "O", "U"]; var msg = '<h2><font color="green">A' + ((vowels.indexOf(tourName.charAt(0).toUpperCase())) > -1) ? 'n ' : ' ' + tourName + ' Tournament is currently in its signup phase. <button onclick="emit(socket, \'chat\', {room: \'' + roomid + '\', message: \'/jt\'});"><b>Join Tournament</b></button></font></h2>';
+		var tourName = tour.tiers[tour[roomid].tier].name; var vowels = ["A", "E", "I", "O", "U"]; var msg = '<h2><font color="green">A' + (((vowels.indexOf(tourName.charAt(0).toUpperCase())) > -1) ? 'n ' : ' ' ) + tourName + ' Tournament is currently in its signup phase. <button onclick="emit(socket, \'chat\', {room: \'' + roomid + '\', message: \'/jt\'});"><b>Join Tournament</b></button></font></h2>';
 		if (user.can('broadcast') && cmd.charAt(0) == "!") {
 			showOrBroadcastStart(user, cmd, room, socket, message);
 			showOrBroadcast(user, cmd, room, socket, msg);
