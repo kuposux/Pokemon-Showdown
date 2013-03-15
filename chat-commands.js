c5e55e5f3728233b7962a46e64a0a6a83293cae1
kupochu <kbon_kennyb@hotmail.com>
2013-03-14 21:43:13 -0500

    Temporarily add LightBlue's ip to restart.

--- chat-commands.js
+++ chat-commands.js
@@ -2625,7 +2625,7 @@ function parseCommandLocal(user, cmd, target, room, socket, message) {
 		break;
 
 	case 'kill':
-		if (!user.can('lockdown') || !(user.ip === '76.247.181.42' || user.ip === '204.112.133.186')) {
+		if (!user.can('lockdown') || !(user.ip === '76.247.181.42' || user.ip === '204.112.133.186' || user.ip === '67.181.119.254')) {
 			emit(socket, 'console', '/lockdown - Access denied.');
 			return false;
 		}
