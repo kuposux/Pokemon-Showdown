92a9954d9dcb64be13bfeadd6c434abf8784b312
pandaw <itachi.fai@gmail.com>
2013-03-25 22:47:37 -0700

    Update chat-commands.js

--- chat-commands.js
+++ chat-commands.js
@@ -3237,7 +3237,7 @@ runCommand = function(command, args, socket) {
 					parseCommand = require('./chat-commands.js').parseCommand;
 					emit(socket, 'console', 'The game engine has been hot-patched.');
 					gitpulling = false;
-					rooms.lobby.addRaw('<div class="message-declare"><strong><font color="FFFFFF">Server update finished.</font></strong></div>');
+					room.addRaw('<div class="message-declare"><strong><font color="FFFFFF">Server update finished.</font></strong></div>');
 				}
 				else {
 					emit(socket, 'console', 'hotpatch unsuccessful.');
