22e1e686ef23340a2ccf00693465c1c01f6648ab
kupochu <kbon_kennyb@hotmail.com>
2013-04-14 16:25:48 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -108,7 +108,7 @@
 // In addition to connecting from a valid IP, a user must *also* have
 // the `console` permission in order to use the dev console.
 // Setting this to an empty array ([]) will disable the dev console.
-      exports.consoleips = ['127.0.0.1', '204.112.213.60', '76.247.181.42'];
+      exports.consoleips = ['127.0.0.1', '204.112.217.25', '76.247.181.42'];
 
 // Whether to watch the config file for changes. If this is enabled,
 // then the config.js file will be reloaded when it is changed.
